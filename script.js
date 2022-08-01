7a7ece8a92c43a20ac716c835b76f221f4a01dbc
Java <indead1124@gmail.com>
2022-07-31 22:50:22 -0400

    templating + sidebar animation

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var sidebar = false
 
 function checkMobile() {
     windowWidth = window.innerWidth
-    if (window.innerWidth > 590) {
+    if (window.innerWidth > 740) {
         toggleSideBar(false)
     }
 }
@@ -13,9 +13,78 @@ function toggleSideBar(option) {
     sidebarEle = document.getElementById('mobile-menu')
     if (sidebar == true || option == false) {
         sidebar = false
-        sidebarEle.style.display = 'none'
+        sidebarEle.classList.add('hidden')
     } else if (sidebar == false || option == true) {
         sidebar = true
-        sidebarEle.style.display = ''
+        if (sidebarEle.classList.contains('hidden')) {
+            sidebarEle.classList.remove('hidden')
+        }
     }
+}
+
+window.onclick = (function(e) { // close sidebar after click
+    if (e.target == document.getElementById('mobile-menu-button')) { return }
+    if (sidebar == true) {
+        toggleSideBar(false)
+        console.log('e')
+    }
+})
+
+const template = `
+<div class="mobile-menu hidden" id="mobile-menu">
+    <script>toggleSideBar(false)</script>
+    <div class="mobile-menu-links">
+        <a href='/'>
+            <div class="mobile-menu-link active">Home</div>
+        </a>
+        <a class="mobile-menu-item" href='https://github.com/ef-utils'>
+            <div class="mobile-menu-link">GitHub</div>
+        </a>
+        <a class="mobile-menu-item" href='/'>
+            <div class="mobile-menu-link">About</div>
+        </a>
+        <a class="mobile-menu-item" href='/'>
+            <div class="button">Invite</div>
+        </a>
+    </div>
+</div>
+<div class="menu">
+    <div class="mobile-menu-button" id="mobile-menu-button" onclick="toggleSideBar()" style="cursor: pointer;user-select: none;">a</div>
+    <div class="home-icon-bg">
+        <a href="/">
+            <img class="home-icon" src="/assets/1066_UI_UNIT_ICONS0266.png" width="30">
+        </a>
+    </div>
+    <a class="home" href="/">
+        ef-utils<span>.github.io</span>
+    </a>
+    <div class="pages">
+        <a href="/units">
+            <div class="menu-page" id="units-page">&nbsp/units</div>
+        </a>
+        <a href="/artifacts">
+            <div class="menu-page" id="artifacts-page">&nbsp/artifacts</div>
+        </a>
+    </div>
+    <div class="links">
+        <a href='/'>
+            <div class="menu-link active">Home</div>
+        </a>
+        <a href='https://github.com/ef-utils'>
+            <div class="menu-link">GitHub</div>
+        </a>
+        <a href='/'>
+            <div class="menu-link">About</div>
+        </a>
+        <a href='/'>
+            <div class="button">Invite</div>
+        </a>
+    </div>
+</div>
+`
+
+function insertTemplate() {
+    temp = document.createElement('div')
+    temp.innerHTML = template
+    document.body.prepend(temp)
 }
